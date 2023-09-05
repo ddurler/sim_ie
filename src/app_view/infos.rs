95e58ccff41d1f5d44e66873f24c519d1d47a4bc
DD <denis.durler@alma-alma.fr>
2023-09-05 18:14:25 +0200

    code (context): Add test

--- src/app_view/infos.rs
+++ src/app_view/infos.rs
@@ -8,6 +8,6 @@ use context::IdInfo;
 
 /// Visualisation IHM de la valeur d'un champ du contexte identifié par son `IdInfo`
 pub fn show_info(id_info: &IdInfo) -> Element<'static, Message> {
-    let txt = format!("{:?}", id_info.clone());
+    let txt = format!("{}", context::get_info_name(id_info).to_string());
     Text::new(txt).into()
 }
--- src/context/mod.rs
+++ src/context/mod.rs
@@ -21,6 +21,7 @@ pub enum FormatInfo {
     FormatBool,
     FormatU8,
     FormatU32,
+    FormatF32,
 }
 
 /// Énumération des informations du contexte
@@ -76,8 +77,23 @@ pub struct Context {
     predetermination: Option<u32>,
 }
 
+/// Retourne le libellé d'un information du contexte
+pub fn get_info_name(id_info: &IdInfo) -> &str {
+    match id_info {
+        IdInfo::EnMesurage => "En mesurage",
+        IdInfo::CodeDefaut => "Code défaut",
+        IdInfo::ArretIntermediaire => "Arrêt intermédiaire",
+        IdInfo::ForcagePetitDebit => "Forçage petit débit",
+        IdInfo::ModeConnecte => "Mode connecté",
+        IdInfo::Totalisateur => "Totalisateur",
+        IdInfo::DebitInstant => "Débit instantané",
+        IdInfo::QuantiteChargee => "Quantité chargée",
+        IdInfo::TemperatureInstant => "Température instantanée",
+        IdInfo::Predetermination => "Prédétermination",
+    }
+}
+
 /// Retourne le type de format pour une information du contexte
-/// # panics
 pub fn get_info_format(id_info: &IdInfo) -> FormatInfo {
     match id_info {
         /* Booléen */
@@ -95,7 +111,7 @@ pub fn get_info_format(id_info: &IdInfo) -> FormatInfo {
         }
 
         /* F32 */
-        IdInfo::DebitInstant | IdInfo::TemperatureInstant => FormatInfo::FormatU32,
+        IdInfo::DebitInstant | IdInfo::TemperatureInstant => FormatInfo::FormatF32,
     }
 }
 
@@ -174,5 +190,62 @@ impl Context {
 
             _ => panic!("Cette information n'est pas f32 : {id_info:?}"),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_set() {
+        fn check_id_code(context: &mut Context, id_info: &IdInfo) {
+            match self::get_info_format(id_info) {
+                FormatInfo::FormatBool => {
+                    assert!(context.get_info_bool(id_info).is_none());
+                    for value in [true, false] {
+                        context.set_info_bool(id_info, value);
+                        assert_eq!(context.get_info_bool(id_info), Some(value));
+                    }
+                }
+                FormatInfo::FormatU8 => {
+                    assert!(context.get_info_u8(id_info).is_none());
+                    for value in [0_u8, 10_u8, 100_u8] {
+                        context.set_info_u8(id_info, value);
+                        assert_eq!(context.get_info_u8(id_info), Some(value));
+                    }
+                }
+                FormatInfo::FormatU32 => {
+                    assert!(context.get_info_u32(id_info).is_none());
+                    for value in [0_u32, 1000_u32, 100_000_u32] {
+                        context.set_info_u32(id_info, value);
+                        assert_eq!(context.get_info_u32(id_info), Some(value));
+                    }
+                }
+                FormatInfo::FormatF32 => {
+                    assert!(context.get_info_f32(id_info).is_none());
+                    for value in [0.0_f32, 1000.0_f32, -1000.0_f32, 100_000.0_f32] {
+                        context.set_info_f32(id_info, value);
+                        assert_eq!(context.get_info_f32(id_info), Some(value));
+                    }
+                }
+            }
+        }
+
+        let mut context = Context::default();
+
+        // Pas besoin de mettre ici tous les IdInfos... Au moins tester les différents formats :)
+        // TODO : Pas réussi à mettre en oeuvre le crate `enum-iterator` qui permettrait d'itérer
+        //        sur toutes les valeurs d'un Enum :(
+        check_id_code(&mut context, &IdInfo::EnMesurage);
+        check_id_code(&mut context, &IdInfo::CodeDefaut);
+        check_id_code(&mut context, &IdInfo::ArretIntermediaire);
+        check_id_code(&mut context, &IdInfo::ForcagePetitDebit);
+        check_id_code(&mut context, &IdInfo::ModeConnecte);
+        check_id_code(&mut context, &IdInfo::Totalisateur);
+        check_id_code(&mut context, &IdInfo::DebitInstant);
+        check_id_code(&mut context, &IdInfo::QuantiteChargee);
+        check_id_code(&mut context, &IdInfo::TemperatureInstant);
+        check_id_code(&mut context, &IdInfo::Predetermination);
     }
 }
